886d2ded5a326514953bac8da28e72ec68eaa1ff
BHASVIC-RobertSimmons23 <145435727+BHASVIC-RobertSimmons23@users.noreply.github.com>
2024-02-08 17:13:50 +0000

    Completed Apartments Exercise

--- src/Part5/ComparingApartmentsExercise/Apartment.java
+++ src/Part5/ComparingApartmentsExercise/Apartment.java
@@ -1,4 +1,25 @@
 package Part5.ComparingApartmentsExercise;
 
 public class Apartment {
+    private final int rooms;
+    private final int squares;
+    private final int pricePerSquare;
+
+    public Apartment(int rooms, int squares, int pricePerSquare) {
+        this.rooms = rooms;
+        this.squares = squares;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment apartment2) {
+        return this.squares > apartment2.squares;
+    }
+
+    public int priceDifference(Apartment apartment2) {
+        return Math.abs(this.squares * this.pricePerSquare - apartment2.squares * apartment2.pricePerSquare);
+    }
+
+    public boolean moreExpensiveThan(Apartment apartment2) {
+        return this.squares * pricePerSquare > apartment2.squares * apartment2.pricePerSquare;
+    }
 }
